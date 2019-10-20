c28f994a770ed8490b9d3a594c152e227f13a58f
progress44 <sinanajani@outlook.com>
2019-10-21 00:16:41 +0200

    [~] Refactor ui to be as generic as possible

--- ui.php
+++ ui.php
@@ -2,35 +2,44 @@
 
 namespace Sink;
 
-use \Sink\Sink;
-
 class UI
 {
-
   protected $optionsTemplate = 'Templates/OptionPage.php';
   protected $noticesTemplate = 'Templates/AdminNotice.php';
+  protected $plugin_name;
 
-  public function __construct()
+  public function __construct($plugin_name)
   {
     if (is_admin()) {
         add_action('admin_menu', [$this, 'addOptionsMenu']);
-        add_filter('plugin_action_links_'.plugin_basename(__FILE__), [$this, 'renderSettingsButton']);
+        add_filter('plugin_action_links_'.$plugin_name, [$this, 'renderSettingsButton']);
     }
+
+    if ($plugin_name) {
+      $this->plugin_name = $plugin_name;
+    }
+  }
+
+  public function pluginNameToClassName()
+  {
+    $class_name = strtoupper(substr($this->plugin_name, 0, 1)).substr($this->plugin_name, 1);
+    return $class_name;
   }
 
   public function renderOptionsPage()
   {
-    $configMap = (new Sink())->configMap;
+    $class = "\\".$this->pluginNameToClassName()."\\".$this->pluginNameToClassName();
+    $configMap = (new $class)->configMap;
     require $this->optionsTemplate;
   }
 
   public function addOptionsMenu()
   {
     add_options_page(
-      'Sink settings',
-      'Sink',
+      $this->pluginNameToClassName().' settings',
+      $this->pluginNameToClassName(),
       'manage_options',
-      'sink',
+      $this->plugin_name,
       [$this, 'renderOptionsPage']
     );
   }
@@ -47,11 +56,15 @@ class UI
 
   public function renderSettingsButton($links)
   {
+    if (!$this->plugin_name) {
+      return $links;
+    }
+
     array_splice(
           $links,
           0,
           0,
-          '<a href="' .admin_url('options-general.php?page=sink') .
+          '<a href="' .admin_url('options-general.php?page='.$this->plugin_name) .
               '">' . __('Settings') . '</a>'
       );
       return $links;
